5a47ab369646ebef9ce932db3697d0a8d2d8dd34
Nobody <nobodydisturbme@gmail.com>
2017-05-10 19:40:09 +0530

    alarm notifi, displays notification at set time in seconds

---
+++ app/src/main/java/com/example/nobodyme/alarm_notifi/MainActivity.java
@@ -0,0 +1,46 @@
+package com.example.nobodyme.alarm_notifi;
+
+import android.app.AlarmManager;
+import android.app.PendingIntent;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button button = (Button)findViewById(R.id.button);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                EditText e1= (EditText)findViewById(R.id.editText);
+                int i = Integer.parseInt(e1.getText().toString());
+                Intent intent = new Intent(MainActivity.this,OurBroadcastReceiver.class);
+                PendingIntent pendingIntent = PendingIntent.getBroadcast(MainActivity.this,23424243, intent, 0);
+                AlarmManager alarmManager = (AlarmManager)getSystemService(ALARM_SERVICE);
+                alarmManager.set(AlarmManager.RTC_WAKEUP, System.currentTimeMillis()+(i*1000), pendingIntent);
+
+
+                Toast.makeText(MainActivity.this, "Alarm set in " + i + " seconds", Toast.LENGTH_LONG).show();
+
+
+
+
+
+
+            }
+        });
+
+
+
+    }
+}
